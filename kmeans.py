36525006fa5d6140c85679ad9643bbaa583460e2
adelina <adelinafigueiraf@gmail.com>
2023-12-02 17:58:52 -0400

    kmeans

--- kmeans.py
+++ kmeans.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from random import uniform
+import random
 
 class Cluster:
 	def __init__(self,center,points):
@@ -11,7 +14,7 @@ class Cluster:
 		self.points.append(point)
 	def __str__(self):
 		points_string = list(map(lambda x: str(x), self.points))
-		return "Cluster center = " + str(self.center)+ " Points = ["+str(points_string)+"]"
+		return "Cluster center = " + str(self.center)+ " Points = ["+str(len(points_string))+"]"
 
 class K_means:
 	def __init__(self,ncluster,file):
@@ -20,9 +23,16 @@ class K_means:
 		self.clusters = [0]*ncluster
 
 	def initialize(self):
-		np.random.seed(23)
+		'''center = [random.choice(self.X[0])]
+		for x in range(self.n-1):
+			dists = np.sum([self.distance_x_y(centroid, self.X) for centroid in self.centroids], axis=0)
+			dists = np.sum(dists)
+			new_centroid_idx, = np.random.choice(range(len(self.X)), size=1, p=dists)
+			self.centroids += [self.X[new_centroid_idx]]'''
 		for x in range(self.n):
-			center = 2*(2*np.random.random((self.X.shape[1],))-1)
+			center = self.X[0] #[0]*(2*np.random.random((self.X.shape[1],))-1)
+			#min_, max_ = np.min(self.X), np.max(self.X)
+			#self.centroids = [X[0,0]]*(self.X.shape[1])
 			self.clusters[x] = Cluster(center,[])
 
 	def distance_x_y(self,x,y):
@@ -34,8 +44,8 @@ class K_means:
 			d = []
 			current_ex = self.X[y]
 
-			for i in range(self.n):
-				distance = self.distance_x_y(current_ex,self.clusters[i].center)
+			for cluster in self.clusters:
+				distance = self.distance_x_y(current_ex,cluster.center)
 				d.append(distance)
 			current_cluster = np.argmin(d)
 			self.clusters[current_cluster].addpoint(current_ex)
@@ -44,23 +54,25 @@ class K_means:
 
 	def updating(self,distances,minimo_local):
 		for i in range(self.n):
+			print("size of points at cluster",i)
+			print(len(self.clusters[i].points))
 			points = np.array(self.clusters[i].points)
 			if points.shape[0] > 0:
-				new = points.mean(axis=0)
+				new = np.mean(points, axis=0) #points.mean(axis=0)
 				self.clusters[i].center = new 
-				if min(distances) > minimo_local:
-					self.clusters[i].points = []
+				self.clusters[i].points = []
 
 	def fit(self):
 		self.initialize()
 		distances = [100]
-		minimo_local = 0.35
-		for it in range(0,100):
+		minimo_local = 0.01
+		cluster = self.clusters
+		for it in range(0,10):
+			if max(distances) < minimo_local:
+				break
 			distances = self.assign()
 			self.updating(distances,minimo_local)
-			print(min(distances))
-			if min(distances) < minimo_local:
-				break
+		print(self.predict())
 
 	def predict(self):
 		p = []
@@ -73,13 +85,12 @@ class K_means:
 
 	def p(self):
 		pred = self.predict()
-		print(pred)
 
 		plt.figure(figsize=(12,5))
 		plt.subplot(1,2,1)
 		plt.scatter(self.X[:,0],self.X[:,1],c = pred)
 		for i in self.clusters:
-			center = i.center[:2]
+			center = i.center
 			plt.scatter(center[0],center[1],marker = '^',c = 'red')
 		plt.xlabel("petal length (cm)")
 		plt.ylabel("petal width (cm)")
@@ -87,8 +98,8 @@ class K_means:
 		plt.subplot(1,2,2)   
 		plt.scatter(self.X[:,2],self.X[:,3],c = pred)
 		for i in self.clusters:
-			center = i.center[2:4]
-			plt.scatter(center[0],center[1],marker = '^',c = 'red')
+			center = i.center
+			plt.scatter(center[2],center[3],marker = '^',c = 'red')
 		plt.xlabel("petal length (cm)")
 		plt.ylabel("petal width (cm)")
 		plt.show() 
@@ -114,11 +125,10 @@ class K_means:
 		return a
 
 
-#X,y = make_blobs(n_samples = 500,n_features = 2,centers = 3,random_state = 23)
+X,y = make_blobs(n_samples = 20,n_features = 4,centers = 5,random_state = 23)
 
 iris = pd.read_csv("iris.csv")
 X = np.array(iris.drop("species",axis=1))
-
 
 #fig = plt.figure(0)
 #plt.grid(True)
@@ -137,12 +147,16 @@ def f(x):
 
 
 
-c = K_means(20,X)
+c = K_means(2,X)
 c.fit()
-#print(c.clusters[0].__str__())
-print(c.clusters[1].__str__())
+#c.p()
+print(c)
 
-print(c.predict())
+
+km = KMeans(n_clusters=2, random_state=2,n_init="auto")
+km.fit(X)
+a = km.predict(X, sample_weight='deprecated')
+print(a)
 '''
 for x in range(4):
 	for y in range(4):
