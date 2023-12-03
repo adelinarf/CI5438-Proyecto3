730313faa2f8dc7a7c7bc2136b0046ab662f0ba8
adelina <adelinafigueiraf@gmail.com>
2023-12-03 01:34:40 -0400

    cambios imagenes

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from sklearn.cluster import KMeans
 from random import uniform
+from numpy import asarray
+
 import random
+from PIL import Image
+
 
 class Cluster:
 	def __init__(self,center,points):
@@ -35,14 +40,16 @@ class K_means:
 			self.clusters[x] = Cluster(center,[])
 
 	def distance_x_y(self,x,y):
-		return np.sqrt(np.sum((x-y)**2)) 
+		potencia=(x-y)**2
+		suma = np.sum(potencia)
+		raiz = np.sqrt(suma)
+		return raiz  #puse el axis para la matriz
 
 	def assign(self):
 		dist = []
 		for y in range(self.X.shape[0]):
 			d = []
 			current_ex = self.X[y]
-
 			for cluster in self.clusters:
 				distance = self.distance_x_y(current_ex,cluster.center)
 				d.append(distance)
@@ -64,11 +71,24 @@ class K_means:
 		distances = [100]
 		minimo_local = 0.01
 		cluster = self.clusters
-		for it in range(0,10):
+		for it in range(0,5):
 			if max(distances) < minimo_local:
+				print("out")
 				break
+			print("assigned")
 			distances = self.assign()
+			print("update")
 			self.updating(distances,minimo_local)
+
+	def pred(self,Y):
+		p = []
+		for i in range(Y.shape[0]):
+			d = []
+			for j in range(self.n):
+				distance = self.distance_x_y(Y[i],self.clusters[j].center)
+				d.append(distance)
+			p.append(np.argmin(d))
+		return p
 
 	def predict(self):
 		p = []
@@ -128,7 +148,7 @@ versicolor = X[50: 100, :]
 virginica = X[100:, :]
 
 
-clusters_test = [1,2,3,4,5,6]
+clusters_test = [] #[1,2,3,4,5,6]
 for cluster in clusters_test:
 	c = K_means(cluster,X)
 	c.fit()
@@ -149,3 +169,58 @@ for cluster in clusters_test:
 	a = km.predict(X, sample_weight='deprecated')
 	print(a)'''
 
+
+def get_image(image_path):
+    image = Image.open(image_path)
+    width, height = image.size
+    pixel_values = list(image.getdata())
+    if image.mode == "RGB":
+        channels = 3
+    elif image.mode=="RGBA":
+    	channels=4
+    elif image.mode == "L":
+        channels = 1
+    else:
+        print("Unknown mode: %s" % image.mode)
+        return None
+    pixel_values = np.array(pixel_values).reshape((width, height, channels))
+    return pixel_values
+
+data = get_image('imagen.jpg')
+
+def change_color(c):
+	if c == 2:
+		return (59, 141, 218)
+	elif c==1:
+		return (59, 218, 83)
+	elif c==0:
+		return (218, 167, 59)
+	return (255, 255, 255)
+
+k=3
+#################################
+#################################
+
+Z=data[0]
+for x in range(1,len(data)):
+	Z = np.vstack((Z, data[x]))
+
+a = K_means(k,Z)
+a.fit()
+prediction = a.predict()
+
+for y in range(len(Z)):
+	Z[y] = change_color(prediction[y])
+
+new = []
+base = 0
+for x in range(data.shape[0]):
+	new.append(Z[base:base+data.shape[1]])
+	base = base+data.shape[1]
+new=np.array(new)
+
+#################################
+#################################
+
+im = Image.fromarray(new.astype(np.uint8), 'RGB')
+im.save("archivo.png")
